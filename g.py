93eed0d2634acca0b6c1fdfdb7b9c4f36c84410e
Your Name <you@example.com>
2018-04-16 13:30:33 +0800

    doc simple

--- g.py
+++ g.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import codecs
 with codecs.open("About this Documentation _ Node.js v8.9.4 Documentation.html","r","utf-8") as fp:
-    soup = BeautifulSoup(fp,"lxml")
+    soup = BeautifulSoup(fp)
     column2=soup.find_all(id='column2')
     #print(len(body))
     #print(type(body),dir(body))
